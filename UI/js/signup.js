d4bfdf13d9dd41e53baa2f10551ddbc7447a0602
kabundege <christophekwizera1@gmail.com>
2020-02-07 08:51:19 +0200

    ```ft(singin):this delivers tha singin and landing pages code```

--- UI/js/signup.js
+++
@@ -1,9 +0,0 @@
-const signin = () =>{
-    return (document.location.href = "../index.html");
-} 
-const dash = () =>{
-    return (document.location.href = "../html/dashboard.html");
-}
-const delete = (id) => {
-    return (document.querySelector(`#${id}`).remove)
-}
