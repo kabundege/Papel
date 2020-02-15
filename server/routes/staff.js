cfd851e5dd21c7d1177538650499d3cf21d9a8ec
kabundege <christophekwizera1@gmail.com>
2020-02-16 01:53:37 +0200

    ft(specificaccAPI): delivers get a specifc account
     - delivers staff/admin should be able to view a specifc bank account
       details using an account number.
    
    [Finishes #171036939]

--- server/routes/staff.js
+++ server/routes/staff.js
@@ -6,4 +6,6 @@ const route = express.Router();
 
 route.get("/api/v1/accounts",auth.access,staffController.accounts);
 
+route.get("/api/v1/accounts/:accountenumber",auth.access,staffController.specificAccount);
+
 export default route;
